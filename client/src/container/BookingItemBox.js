2659c0456f0d270d58aacf69f4c5b1bdc84b7a00
Cameron <cameron.beckit@gmail.com>
2020-02-02 13:22:57 +0000

    components display

---
+++ client/src/container/BookingItemBox.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class BookingItemBox extends Component{
+    render(){
+        return(
+            <div>
+                {/* <DateFilter></DateFilter>
+                <BookingList></BookingList> */}
+                <p>Booking Item List</p>
+            </div>
+        )
+    }
+}
+
+export default BookingItemBox;
