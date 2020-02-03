743bee2b209ebf38f001b93cd4c8e903e6a1a91f
MaryBobs <55357115+MaryBobs@users.noreply.github.com>
2020-02-03 11:04:15 +0000

    completes newCustomer form

--- client/src/component/newBookingComponents/NewCustomerForm.js
+++ client/src/component/newBookingComponents/NewCustomerForm.js
@@ -7,11 +7,13 @@ class NewCustomerForm extends Component {
         this.state = {
             firstName: "",
             surname: "",
-            phoneNumber: ""
+            phoneNumber: "",
+            email: ""
             }
         this.handleFirstName = this.handleFirstName.bind(this);
-        this.handleSurname = this.handleSurname.bind(this);
+        this.handleLastName = this.handleLastName.bind(this);
         this.handlePhoneNumber = this.handlePhoneNumber.bind(this);
+        this.handleEmail = this.handleEmail.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
@@ -19,20 +21,25 @@ class NewCustomerForm extends Component {
         this.setState({firstName: e.target.value});
     }
 
-    handleSurname(e) {
-        this.setState({surname: e.target.value});
+    handleLastName(e) {
+        this.setState({lastName: e.target.value});
     }
 
     handlePhoneNumber(e) {
         this.setState({phoneNumber: e.target.value});
     }
 
+    handleEmail(e) {
+        this.setState({email: e.target.value});
+    }
+
     handleSubmit(e) {
         e.preventDefault();
         const newCustomer = {
             firstName: this.state.firstName,
-            surname: this.state.surname,
-            phoneNumber: this.state.phoneNumber
+            lastName: this.state.lastName,
+            phoneNumber: this.state.phoneNumber,
+            email: this.state.email
         }
         this.props.addCustomer(newCustomer);
         }
@@ -44,9 +51,9 @@ class NewCustomerForm extends Component {
         <h2>New Customer</h2>
         <form onSubmit={this.handleSubmit}>
             <input type="text" placeholder="First Name:" name="firstName" onChange={this.handleFirstName} value={this.state.firstName}/>
-            <input type="text" placeholder="Surname:" name="surname" onChange={this.handleSurname} value={this.state.surname}/>
+            <input type="text" placeholder="Last Name:" name="lastName" onChange={this.handleLastName} value={this.state.lastName}/>
             <input type="number" placeholder="Phone Number:" name="phoneNumber" onChange={this.handlePhoneNumber} value={this.state.phoneNumber}/>
-            <input type="email" placeholder="E-mail:" />
+            <input type="email" placeholder="E-mail:" name="email" onChange={this.handleEmail} value={this.state.email}/>
             <button type="submit">Next</button>
         </form>
         </div>
