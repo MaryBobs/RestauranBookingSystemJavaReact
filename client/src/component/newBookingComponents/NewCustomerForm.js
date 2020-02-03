ffe2ca9113878f0254b93efc9c0a36b5af1b6468
MaryBobs <55357115+MaryBobs@users.noreply.github.com>
2020-02-03 09:40:30 +0000

    working on newCustomer form

--- client/src/component/newBookingComponents/NewCustomerForm.js
+++ client/src/component/newBookingComponents/NewCustomerForm.js
@@ -5,9 +5,11 @@ class NewCustomerForm extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            firstName: ""
-        }
+            firstName: "",
+            surname: ""
+            }
         this.handleFirstName = this.handleFirstName.bind(this);
+        this.handleSurname = this.handleSurname.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
@@ -15,10 +17,15 @@ class NewCustomerForm extends Component {
         this.setState({firstName: e.target.value});
     }
 
+    handleSurname(e) {
+        this.setState({surname: e.target.value});
+    }
+
     handleSubmit(e) {
         e.preventDefault();
         const newCustomer = {
-            firstName: this.state.firstName
+            firstName: this.state.firstName,
+            surname: this.state.surname
         }
         this.props.addCustomer(newCustomer);
         }
@@ -30,7 +37,7 @@ class NewCustomerForm extends Component {
         <h2>New Customer</h2>
         <form onSubmit={this.handleSubmit}>
             <input type="text" placeholder="First Name:" name="firstName" onChange={this.handleFirstName} value={this.state.firstName}/>
-            <input type="text" placeholder="Surname:" />
+            <input type="text" placeholder="Surname:" name="surname" onChange={this.handleSurname} value={this.state.surname}/>
             <input type="number" placeholder="Phone Number:" />
             <input type="email" placeholder="E-mail:" />
             <button type="submit">Next</button>
