283bcf6481fc5a4ebafe15de18eaed625dbcaa69
naz_liton <nazrulliton@icloud.com>
2020-02-03 13:17:42 +0000

    show all page working, button added

--- client/src/component/BookingSystemComponents/Bookings.js
+++ client/src/component/BookingSystemComponents/Bookings.js
@@ -1,26 +1,38 @@
 import React from 'react';
 
 const Bookings = (props) => {
-    const bookingsList = props.Bookings.map(
-        bookings => {
-            return (
-                <table>
-                    <tr>
-            <th>{keys}</th>
-                    </tr>
+    if (props.bookings.length === 0) return null;
 
+    const bookingsList = props.bookings.map(
+        booking => {
+            return (
                     <tr>
-                <td>{value}</td>
+                        <td>{booking.kidsCovers}</td>
+                        <td>{booking.adultsCovers}</td>
+                        <td>{booking.customer.firstName} {booking.customer.lastName}</td>
+                        <td>{booking.date}</td>
+                        <td><button>Manage</button></td>
                     </tr>
-                </table>
             )
         }
  )
     
-    
-    
     return(
-        {bookingsList}
+        <table>
+            <thead>
+                <tr>
+                    <td>Kids Covers</td>
+                    <td>Adults Covers</td>
+                    <td>Customer Name</td>
+                    <td>Date</td>
+
+
+                </tr>
+            </thead>
+            <tbody>
+                {bookingsList}
+            </tbody>
+        </table>
         )
 }
 
