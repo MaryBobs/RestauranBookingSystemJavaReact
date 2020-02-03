f84540a964c0caaf0b971c171362330df182f516
Cameron <cameron.beckit@gmail.com>
2020-02-03 17:00:33 +0000

    adds a fix to the things that were needing fixed

--- client/src/container/BookingSystemBox.js
+++ client/src/container/BookingSystemBox.js
@@ -1,40 +1,65 @@
-import React, {Component} from 'react';
-import Bookings from '../component/BookingSystemComponents/Bookings';
+import React, {Fragment, Component} from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import NewBookingBox from './NewBookingBox.js';
+import NavBar from '../component/NavBar.js';
 import Request from '../helpers/request';
+import BookingPage from '../component/bookingSystemComponents/BookingPage';
+import ShowBooking from '../component/bookingSystemComponents/ShowBooking';
 
 class BookingSystemBox extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            bookings: []
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      bookings: []
     }
+  }
 
-    componentDidMount(){
-        const request = new Request();
+  componentDidMount() {
+    const request = new Request();
 
-        request.get('http://localhost:8080/bookings')
-        .then((data) => {
-            this.setState({bookings: data._embedded.bookings})
-        })
-    }
+    request.get('http://localhost:8080/bookings')
+      .then((data) => {
+        this.setState({ bookings: data._embedded.bookings })
+      })
+  }
 
-    findBookingById(id){
-        return this.state.bookings.find((booking) => {
-            return booking.id === parseInt(id)
-        });
-    }
+  findBookingById(id) {
+    return this.state.bookings.find((booking) => {
+      return booking.id === parseInt(id)
+    });
+  }
 
 
-    render (){
-        return (
-            <div>
-                <h1>Upcoming Bookings</h1>
-                <Bookings bookings = {this.state.bookings}/>
+  render() {
+    return (
+      <div>
+        <h1>Upcoming Bookings</h1>
+        <Router>
+          <Fragment>
+            <NavBar />
+            <Switch>
+              <Route exact path="/bookings">
+                <BookingPage bookings={this.state.bookings} />
+              </Route>
+              <Route path="/bookings/:id" render={routeProps => {
+                return (
+                  <ShowBooking booking={this.state.bookings.find(booking => {
+                    return booking.id === parseInt(routeProps.match.params.id);
+                  })} />
+                )
+              }}>
                 
-            </div>
-        )
-    }
+                </Route>
+              <Route path="/newbooking" component={NewBookingBox} />
+            </Switch>
+
+
+          </Fragment>
+        </Router>
+
+      </div>
+    )
+  }
 }
 
 export default BookingSystemBox;
