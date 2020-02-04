b0598557c12ae82d1a7495d8f33f10852e30f17f
MaryBobs <55357115+MaryBobs@users.noreply.github.com>
2020-02-04 14:28:59 +0000

    edit booking

--- client/src/component/BookingSystemComponents/EditBookingForm.js
+++ client/src/component/BookingSystemComponents/EditBookingForm.js
@@ -1,29 +1,51 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-const EditBookingForm = ({booking, handleBooking}) => {
+class EditBookingForm extends Component {
 
-        if (!booking) {
-            return "Loading!"
+    constructor(props){
+        super(props);
+        this.state = {
+            customer: this.props.booking.customer
         }
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+        // if (!booking) {
+        //     return "Loading!"
+        // }
         
+        handleSubmit(e) {
+            e.preventDefault();
+
+            const booking = {
+                date: e.target.date.value,
+                time: e.target.time.value,
+                kidsCovers: e.target.kidsCovers.value,
+                adultsCovers: e.target.adultsCovers.value,
+                customer: this.customer
+            }
+            this.props.handleUpdate(booking, this.props.booking.id)
+        }
+
+        render() {
         return(
             <div>
             <h1>Edit Booking</h1>
-            <h2>Booking ID: {booking.id}</h2>
-            <form onSubmit={handleBooking}>
+            {/* <h2>Booking ID: {this.props.booking.id}</h2> */}
+            <form onSubmit={this.handleSubmit}>
                 <label>Date:</label>
-            <input type="date" id="date" defaultValue={booking.date}/>
+            <input type="date" name="date" defaultValue={this.props.booking.date}/>
             <label>Time:</label>
-            <input type="time" step="900" name="time" defaultValue={booking.time}/>
+            <input type="time" step="900" name="time" defaultValue={this.props.booking.time}/>
             <label>Adults Covers:</label>
-            <input type="number" placeholder="Adults:" name="adultsCovers" defaultValue={booking.adultsCovers}/>
+            <input type="number" placeholder="Adults:" name="adultsCovers" defaultValue={this.props.booking.adultsCovers}/>
             <label>Kids Covers:</label>
-            <input type="number" placeholder="Children:" name="kidsCovers" defaultValue={booking.kidsCovers}/>
+            <input type="number" placeholder="Children:" name="kidsCovers" defaultValue={this.props.booking.kidsCovers}/>
             <button type="submit">Save Changes to Booking</button>
         </form>
             </div>
         )
-    
+        }
 }
 
 export default EditBookingForm;
