b973ad4cd69fb8498ec2ddc69feb0f7c9d88993d
naz_liton <nazrulliton@icloud.com>
2020-02-05 11:23:13 +0000

    barchart div created

--- client/src/container/BookingSystemBox.js
+++ client/src/container/BookingSystemBox.js
@@ -19,7 +19,7 @@ class BookingSystemBox extends Component {
       searchedDate: "",
       chartdata: [],
       hours: ["12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00",],
-      barchartdata = []
+      barchartdata: []
       
     }
 
@@ -162,11 +162,7 @@ class BookingSystemBox extends Component {
             
         </Switch>
       </Fragment>
-    </Router>
-          
-         
-          
-          
+    </Router>  
 
       </div>
     )
