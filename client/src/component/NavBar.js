01ff49919786b9e695649409efd49800dfe0a0ca
Cameron <cameron.beckit@gmail.com>
2020-02-05 11:06:32 +0000

    adds navbar styling

--- client/src/component/NavBar.js
+++ client/src/component/NavBar.js
@@ -5,7 +5,7 @@ const NavBar = (props) => {
         <div class="nav">
             <ul>
                 <li><a href="/" class="nav-link">Home</a></li>
-                <li><a href="/newbooking" class="nav-link">New Booking</a></li>
+                <li><a href="/newbooking" class="nav-link">New Bookdasdasdaing</a></li>
                 <li><a href="/bookings" class="nav-link"><em>Bookings</em></a></li>   
             </ul>
         </div>
