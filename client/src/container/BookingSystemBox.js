609b4cf045022ff7f7798d0a25088497cf962e69
MaryBobs <55357115+MaryBobs@users.noreply.github.com>
2020-02-05 09:45:41 +0000

    fixes chart data waring

--- client/src/container/BookingSystemBox.js
+++ client/src/container/BookingSystemBox.js
@@ -17,7 +17,7 @@ class BookingSystemBox extends Component {
       bookings: [],
       filteredBookings: [],
       searchedDate: "",
-      chartdata: [],
+      chartdata: {},
       hours: ["12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00"]
     }
 
