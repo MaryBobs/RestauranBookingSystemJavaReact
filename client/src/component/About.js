08fd5fe96c2759f93d5b9abcc3caaaad05980483
naz_liton <nazrulliton@icloud.com>
2020-02-05 19:12:38 +0000

    About info text altered

--- client/src/component/About.js
+++ client/src/component/About.js
@@ -7,7 +7,7 @@ import '../App.css';
             return (
             <div className="about" >
                 <h2>Dino-S'mores Booking Management system</h2>
-                <h4> Who We Are</h4>
+                <h4> Foods With Allergies</h4>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ante ex, vulputate ut ex nec, molestie ultrices lorem. Mauris vestibulum mollis mauris, ac placerat magna scelerisque et. In quis arcu non dui luctus laoreet semper in massa. Cras lacinia at mauris a tincidunt. Fusce ultrices ex in pretium rutrum. Morbi eleifend, purus eget pulvinar imperdiet, mi lectus mollis diam, vitae sodales urna neque ac arcu. Nulla dictum sed tellus eu lobortis. Fusce eu posuere nisl, vel elementum justo. Nunc sit amet sem in est consectetur accumsan. Nunc vel orci suscipit, molestie risus vel, pretium sem.</p>
                 <h4>Our Values</h4>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ante ex, vulputate ut ex nec, molestie ultrices lorem. Mauris vestibulum mollis mauris, ac placerat magna scelerisque et. In quis arcu non dui luctus laoreet semper in massa. Cras lacinia at mauris a tincidunt. Fusce ultrices ex in pretium rutrum. Morbi eleifend, purus eget pulvinar imperdiet, mi lectus mollis diam, vitae sodales urna neque ac arcu. Nulla dictum sed tellus eu lobortis. Fusce eu posuere nisl, vel elementum justo. Nunc sit amet sem in est consectetur accumsan. Nunc vel orci suscipit, molestie risus vel, pretium sem.</p>
