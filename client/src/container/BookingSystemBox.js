e5b779accd81ad358264e18b1ef47c34f476eff5
naz_liton <nazrulliton@icloud.com>
2020-02-05 10:09:40 +0000

    fluff added

--- client/src/container/BookingSystemBox.js
+++ client/src/container/BookingSystemBox.js
@@ -163,8 +163,12 @@ class BookingSystemBox extends Component {
     </Router>
           <div className="about">
           <h2>About us</h2>
-          
-
+          <h4> Who We Are</h4>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ante ex, vulputate ut ex nec, molestie ultrices lorem. Mauris vestibulum mollis mauris, ac placerat magna scelerisque et. In quis arcu non dui luctus laoreet semper in massa. Cras lacinia at mauris a tincidunt. Fusce ultrices ex in pretium rutrum. Morbi eleifend, purus eget pulvinar imperdiet, mi lectus mollis diam, vitae sodales urna neque ac arcu. Nulla dictum sed tellus eu lobortis. Fusce eu posuere nisl, vel elementum justo. Nunc sit amet sem in est consectetur accumsan. Nunc vel orci suscipit, molestie risus vel, pretium sem.</p>
+          <h4>Our Values</h4>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ante ex, vulputate ut ex nec, molestie ultrices lorem. Mauris vestibulum mollis mauris, ac placerat magna scelerisque et. In quis arcu non dui luctus laoreet semper in massa. Cras lacinia at mauris a tincidunt. Fusce ultrices ex in pretium rutrum. Morbi eleifend, purus eget pulvinar imperdiet, mi lectus mollis diam, vitae sodales urna neque ac arcu. Nulla dictum sed tellus eu lobortis. Fusce eu posuere nisl, vel elementum justo. Nunc sit amet sem in est consectetur accumsan. Nunc vel orci suscipit, molestie risus vel, pretium sem.</p>
+          <h4>Our Vision</h4>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ante ex, vulputate ut ex nec, molestie ultrices lorem. Mauris vestibulum mollis mauris, ac placerat magna scelerisque et. In quis arcu non dui luctus laoreet semper in massa. Cras lacinia at mauris a tincidunt. Fusce ultrices ex in pretium rutrum. Morbi eleifend, purus eget pulvinar imperdiet, mi lectus mollis diam, vitae sodales urna neque ac arcu. Nulla dictum sed tellus eu lobortis. Fusce eu posuere nisl, vel elementum justo. Nunc sit amet sem in est consectetur accumsan. Nunc vel orci suscipit, molestie risus vel, pretium sem.</p>
           </div>
           <div className="Upcoming">
           <h2>Upcoming Events</h2>
